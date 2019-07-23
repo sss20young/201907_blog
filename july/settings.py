e615730eb32b463d73ec037395992a9839650168
sss20young <ksyt98@gmail.com>
2019-07-23 21:07:33 +0900

    heroku

--- july/settings.py
+++ july/settings.py
@@ -131,5 +131,5 @@ STATICFILES_DIRS = [
 ]
 
 
-db_from env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
